69fb37ae51e88660b5c0e1856b502613e1b48b77
Mikhail <mikhailkhudoshin@gmail.com>
2022-11-10 17:51:30 +0400

    feat: add styles mudules

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Header />
         <h1>Главная</h1>
         <Link href="/burgers">Все бургеры</Link>
-      <Footer />
     </div>
-  )
-}
+  );
+};
