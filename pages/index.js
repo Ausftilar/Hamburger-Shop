c44b730b43fa0d79bb33da84126292eb0e1e1b00
Mikhail <mikhailkhudoshin@gmail.com>
2022-11-10 21:50:47 +0400

    feat: add coments users

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,20 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
-        <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
-        <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
-        <Link href="/burgers" className={styles.btn}>Все бургеры</Link>
-    </div>
+    <>
+      <Head>
+        <title>Жирные бургеры | Главная</title>
+        <meta name="title" content="Жирные бургеры"/>
+      </Head>
+      <div className={styles.container}>
+          <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
+          <div className={styles.mainImage}>
+            <Image src="/fatburger.png" alt="fat burger" width={400} height={300} priority={true} />
+          </div>
+          <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
+          <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
+          <Link href="/burgers" className={styles.btn}>Все бургеры</Link>
+      </div>
+    </>
   );
 };
